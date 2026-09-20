d0c4a2e0b16b99cf93fe254d39d9e0d7b0750666
Peter Verveer <pieter.verveer@tno.nl>
2026-09-20 22:00:02 +0200

    Name the backend configuration section correctly

--- src/ropt_eo/everest_optimizers.py
+++ src/ropt_eo/everest_optimizers.py
@@ -60,12 +60,12 @@ class EverestOptimizers(Backend):
         [`external`][ropt.backend.external.ExternalBackend] backend.
 
     To select an optimizer, set the `method` field within the
-    [`optimizer`][ropt.config.BackendConfig] section of the
+    [`backend`][ropt.config.BackendConfig] section of the
     [`EnOptContext`][ropt.context.EnOptContext] configuration object to the desired
     algorithm's name. Most methods support the general options defined in the
     [`EnOptContext`][ropt.context.EnOptContext] object. For algorithm-specific
     options, use the `options` dictionary within the
-    [`optimizer`][ropt.config.BackendConfig] section.
+    [`backend`][ropt.config.BackendConfig] section.
 
     Below are the supported options:
 
